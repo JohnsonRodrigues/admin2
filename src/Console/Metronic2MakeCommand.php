0f7b10b63780e10cf9f4afb4b5ffd4c7a7e8113f
JOHNSON RODRIGUES <johnsonrodrigues19@gmail.com>
2019-07-14 18:02:48 -0300

    first commit

--- src/Console/Metronic2MakeCommand.php
+++ src/Console/Metronic2MakeCommand.php
@@ -11,12 +11,12 @@ class Metronic2MakeCommand extends AuthMakeCommand
     protected $description = 'Scaffold basic Metronic2 login and registration views and routes';
 
     protected $metronicViews = [
-        'auth/login.stub'           => 'auth/login.blade.php',
-        'auth/register.stub'        => 'auth/register.blade.php',
-        'auth/passwords/email.stub' => 'auth/passwords/email.blade.php',
-        'auth/passwords/reset.stub' => 'auth/passwords/reset.blade.php',
-        'auth/verify.stub'          => 'auth/verify.blade.php',
-        'home.stub'                 => 'home.blade.php',
+        'auth/login.stub'           => 'auth/clube/login.blade.php',
+        'auth/register.stub'        => 'auth/clube/register.blade.php',
+        'auth/passwords/email.stub' => 'auth/clube/passwords/email.blade.php',
+        'auth/passwords/reset.stub' => 'auth/clube/passwords/reset.blade.php',
+        'auth/verify.stub'          => 'auth/clube/verify.blade.php',
+        'home.stub'                 => 'clube/home.blade.php',
     ];
 
     protected function exportViews()
